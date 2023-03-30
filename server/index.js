4a4ef8e9d1346e382201a17284666104851c2ac6
deepakgunpal <gunpaldeepak@gmail.com>
2023-03-30 21:30:50 +0530

    added yt link

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// https://youtu.be/djMy4QsPWiI?list=RDCMUC8S4rDRZn6Z_StJ-hh7ph8g
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -9,18 +10,18 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log("user connected ==>", socket.id);
-    socket.on("sendMessage", (message) => {
-        console.log("message =>", message)
-        socket.broadcast.emit("receiveMessage", message);
-    })
-})
+  console.log("user connected ==>", socket.id);
+  socket.on("sendMessage", (message) => {
+    console.log("message =>", message);
+    socket.broadcast.emit("receiveMessage", message);
+  });
+});
 
 server.listen(3001, () => console.log("listening on 3001"));
